28350ba7ea7e68fc98545024cf183c02862f4161
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2018-12-06 00:36:51 +0900

    [#2] [Update] Add simple webserver

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/hrzon/go-vue-boilerplate/internal/app/webserver"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	http.HandleFunc("/", webserver.RootHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
