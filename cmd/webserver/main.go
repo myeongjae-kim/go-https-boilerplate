0d4baebfd8fa4dffc35601af9f00f1c44ba6830c
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2018-12-10 01:43:52 +0900

    [#2] Add simple http server

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/hrzon/go-vue-boilerplate/internal/app/webserver"
+	"github.com/hrzon/go-vue-boilerplate/pkg/logger"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	logFile, err := logger.InitLoggerWithLogFileName("log")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer logFile.Close()
 
 	http.HandleFunc("/", webserver.RootHandler)
+
+	//TODO: Give an option to run a server with port number 8080 or 80
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- internal/app/webserver/handlers.go
+++ internal/app/webserver/handlers.go
@@ -1,7 +1,37 @@
 package webserver
 
-import "net/http"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
+const (
+	root = "./web/dist/"
+)
+
+// RootHandler is an handler to send static files
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
+	path := r.URL.Path[len("/"):]
+
+	source, err := ioutil.ReadFile(root + path)
+	if err != nil {
+		source, err = ioutil.ReadFile(root + path + "/index.html")
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		path += "index.html"
+	}
+
+	// Set content type to css if required file's extension is css.
+	if len(path) >= 4 && path[len(path)-4:] == ".css" {
+		w.Header().Set("Content-Type", "text/css")
+	}
+
+	w.Write(source)
+
+	//TODO: Log more detailed information.
+	log.Println("(rootHandler) The requested file has been sent: ", root+path)
 }
